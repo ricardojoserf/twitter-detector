340ccccd8616a1913e10411116f996861bf27424
Ricardo Ruiz <rjruizfdez@gmail.com>
2017-08-23 13:38:35 +0200

    Searching in location/query. Alerts program added

---
+++ alerts.py
@@ -0,0 +1,46 @@
+import csv, operator, argparse
+
+users_file = 'results/users.csv'
+low_limit = 0
+max_users = 5
+
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-o', '--option', required=True, action='store', help='Option')
+	parser.add_argument('-n', '--number', required=True, action='store', help='Number')
+	my_args = parser.parse_args()
+	return my_args
+
+def top(users_sorted, max_users):
+	for user in reversed(users_sorted[(-max_users):]):
+		print 'User:%s   \tValue:%s' % (user[0],user[1])
+
+
+def lim(users_sorted, limit):
+	for user in reversed(users_sorted):
+		if user[1] > limit:
+			print 'User:%s   \tValue:%s' % (user[0],user[1])
+		else:
+			break
+
+def main():
+	csvFile = open(users_file, 'rb')
+	csvReader = csv.reader(csvFile)
+	all_csv = [line for line in csvReader]
+	users = {}
+	for row in all_csv[1:]:
+		if row[0] in users:
+			users[row[0]] = int( users[row[0]] ) + int( row[1] )
+		else:
+			users[row[0]] = int(row[1])
+	users_sorted = sorted(users.items(), key=lambda x: x[1] )
+	args = get_args()
+	if args.option == "top":
+		top(users_sorted, int(args.number))
+	else:
+		lim(users_sorted, int(args.number))
+
+
+if __name__ == "__main__":
+    main()
--- tweets.py
+++ tweets.py
@@ -18,14 +18,14 @@ def timestamp(text):
 
 def get_args():
 	parser = argparse.ArgumentParser()
-	#parser.add_argument('-q', '--query', required=True, action='store', help='Query')
+	parser.add_argument('-q', '--word', required=False, action='store', help='Word')
+	parser.add_argument('-l', '--location', required=False, action='store', help='Location')
 	my_args = parser.parse_args()
 	return my_args
 
 
 def get_words():
 	res = []
-	#words_array = genfromtxt(words_file, delimiter=',',dtype=None)
 	words_array = recfromcsv(words_file, delimiter=',',dtype=None)
 	for line in words_array:
 		for word in line:
@@ -56,29 +56,6 @@ def logger_add_line(data):
 
 
 def csv_add_line(data):
-
-	'''
-	csvFile = open(users_file, 'rb')
-	csvReader = csv.reader(csvFile)
-	all_csv = [l for l in csvReader]
-	new_val = 0
-	count = 1
-	
- 	for row in all_csv:
- 		count += 1
-		if str(row[0]) == str(data[0]):
-	 		new_val = int(data[1]) + int(row[1])
-	 		print str(new_val)
-	 		print (all_csv[count][1])
-	 		print count
-	 		all_csv[count][1] = str(new_val)
-	 		print (all_csv[count][1])
-	 		csvFile = open(users_file, 'w')
-			csvWriter = csv.writer(csvFile)
-			csvWriter.writerows(all_csv)
-	 		return
-	'''
-
 	csvFile = open(users_file, 'a')
  	csvWriter = csv.writer(csvFile)
 	csvWriter.writerow(data)
@@ -117,11 +94,20 @@ class StdOutListener(tweepy.StreamListener):
 def check_tweets(api, args):
 	l = StdOutListener()
 	myStream = tweepy.Stream(api.auth, l)
-	words = get_words()
-	track_word = words[0]
-	timestamp("Searching for tweets (with %s)" % track_word)
-	myStream.filter(track=[track_word])
-
+	args = get_args()
+	q = args.word
+	loc = args.location
+	if q is not None:
+		track_word = q
+		timestamp("Searching for tweets (with %s)" % track_word)
+		myStream.filter(track=[track_word], async=True)
+	else if loc is not None:
+		location = [float(x) for x in loc.split(",")]
+		timestamp("Searching for tweets (location: %s)" % location)
+		myStream.filter(locations=location, async=True)
+	else:
+		print "Use -l for location or -q for searching query"
+		
 
 def main():	
 	args = get_args()
@@ -129,6 +115,7 @@ def main():
 	for api_file in api_files_list:
 		api = generate_api(api_file)
 		check_tweets(api, args)
+
 
 
 if __name__ == "__main__":
